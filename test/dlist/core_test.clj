48cdce6a4ae0c3058323006ec92f6d2dd6e33b21
Jan Stępień <jstepien@users.sourceforge.net>
2016-03-02 08:46:49 +0100

    Generatively test nested dlists

--- test/dlist/core_test.clj
+++ test/dlist/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.walk :as walk]
             [dlist.core :as d]))
 
 (defspec t-list
@@ -15,3 +16,22 @@
      ys (gen/list gen/int)]
     (= (concat xs ys)
        (seq (d/concat (d/list xs) (d/list ys))))))
+
+(def gen-tree
+  (gen/recursive-gen gen/vector
+                     (gen/fmap list gen/int)))
+
+(def tree->seq flatten)
+
+(defn tree->dlist [tree]
+  (walk/postwalk (fn [form]
+                   (cond (list? form)   (d/list form)
+                         (= [] form)    (d/list)
+                         (vector? form) (reduce d/concat form)
+                         :else          form))
+                 tree))
+
+(defspec t-recursive
+  (prop/for-all
+    [tree gen-tree]
+    (= (tree->seq tree) (seq (tree->dlist tree)))))
